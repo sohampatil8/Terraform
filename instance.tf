ac2897b8605115a26d1312d3656f43b8a753717d
sohamVS <sahilsapate08@gmail.com>
2024-05-31 14:14:18 +0530

    updated

--- instance.tf
+++ instance.tf
@@ -10,3 +10,21 @@ resource "aws_instance" "example_server" {
     Name = "soham_instance"
   }
 }
+
+resource "aws_vpc" "example" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "my-subnet"
+  }
+}
