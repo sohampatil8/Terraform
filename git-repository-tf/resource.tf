1f050b54fa808b157760770e78793a75b79a9d24
sohamVS <sahilsapate08@gmail.com>
2024-06-01 19:22:30 +0530

    first-repo

--- git-repository-tf/resource.tf
+++ git-repository-tf/resource.tf
@@ -1,5 +1,5 @@
 resource "github_repository" "test-terraform" {
-  name        = "testing-=terraform-repo"
+  name        = "testing-terraform-repo"
   description = "Testing purpose"
   visibility = "public"
 
